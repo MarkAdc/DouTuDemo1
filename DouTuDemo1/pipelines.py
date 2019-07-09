b3c595cde397920ee8fa93325db528e07fcbf3f8
mk <mark@qq.com>
2019-07-09 21:50:57 +0800

    Mark

--- DouTuDemo1/pipelines.py
+++ DouTuDemo1/pipelines.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import os
 
 
 class Doutudemo1Pipeline(object):
     def process_item(self, item, spider):
         path = item['img_name']
         body = item['img']
+        if not os.path.exists("imgs"):
+            os.mkdir("imgs")
         with open('imgs/{}.jpg'.format(path), 'wb') as f:
             f.write(body)
